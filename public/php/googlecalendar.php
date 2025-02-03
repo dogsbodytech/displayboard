c73e6c7765ca5027a6d222836360524bc063cb9e
Dan Benton <dan@dogsbody.com>
2025-02-03 23:48:30 +0000

    Crosses everything

--- public/php/googlecalendar.php
+++ public/php/googlecalendar.php
@@ -3,89 +3,30 @@
 /*
  * Description: Get data from private Google Calendar.
  *     The script has a few additional advantages
- *       - We are caching the last results so we don't get rate limited
+ *       - We are caching the last results so we don't run up a bill 
  *       - We can hide our API key in this script instead of the website
- *       - We add a header to show when the data is old
  *
  * Configuration:  The following variables can be set
- *     $apiKey      = The API Key
  *     $calendarId  = ID of Calendar to look at
+ *     $apiKeyFile  = Path to Service Account Key file
  *     $cacheFile   = Path to the cache file
  *     $cacheTime   = How many seconds to cache the response
  *
  */
 
-$apiKey = "NotSet";
-$calendarId = 'NotSet';
+$calendarId = 'sysadmin@dogsbody.com';
+$apiKeyFile = "../../config/googlecalendar.json.config";
 $cacheFile = '../../cache/googlecalendar.cache';
 $cacheTime = 900;  // 15 minutes
 
-include '../../config/googlecalendar.config';
+include '../../config/googlecalendar.config'; // shouldn't be needed 
 
-// If apiKey is not set then don't even try doing anything
-if ($apiKey == "NotSet" || $calendarId == "NotSet") {
+// If $apiKeyFile doesn't exist then don't even try doing anything
+if (!file_exists($apiKeyFile)) {
     http_response_code(500);
     echo json_encode(["success" => "false","error" => "Script not configured"]);
     exit;
 }
-
-require '../../google-api-php-client/vendor/autoload.php';
-
-//use Google\Client;
-//use Google\Service\Calendar;
-
-session_start();
-$client = new Google_Client();
-$client->setAuthConfig('../../config/googlecalendar.json.config');
-$client->setScopes('https://www.googleapis.com/auth/calendar.readonly');
-$client->setApplicationName("Displayboard");
-$service = new Google_Service_Calendar($client);
-
-//$calendarId = 'primary';
-  $optParams = array(
-    'maxResults' => 10,
-    'orderBy' => 'startTime',
-    'singleEvents' => TRUE,
-    'timeMin' => date('c'),
-  );
-
-$results = $service->events->listEvents($calendarId, $optParams);
-
-if (count($results->getItems()) == 0) {
-    print "No upcoming events found.\n";
-  } else {
-    print "Upcoming events:\n";
-    foreach ($results->getItems() as $event) {
-      $start = $event->start->dateTime;
-      if (empty($start)) {
-        $start = $event->start->date;
-      }
-      printf("%s (%s)\n", $event->getSummary(), $start);
-    }
-  }
-
-
-
-
-//$calendar = $service->calendars->get('primary');
-
-//echo $calendar->getSummary();
-
-
-
-
-
-
-
-
-
-/*
-
-
-$calendar = $service->calendars->get('primary');
-
-echo $calendar->getSummary();
-
 
 // Return cached files if exists and is still valid
 if (file_exists($cacheFile) && (time() - filemtime($cacheFile)) < $cacheTime) {
@@ -94,15 +35,43 @@ if (file_exists($cacheFile) && (time() - filemtime($cacheFile)) < $cacheTime) {
     exit;
 }
 
-// Fetch events from Google Calendar API
-$url = "https://www.googleapis.com/calendar/v3/calendars/" . urlencode($calendarId) . "/events?key=$apiKey&timeMin=" . urlencode(gmdate('c')) . "&maxResults=10&orderBy=startTime&singleEvents=true";
-$response = file_get_contents($url);
+require '../../google-api-php-client/vendor/autoload.php';
 
-if ($response === FALSE) {
-    http_response_code(500);
-    echo json_encode(["error" => "Failed to fetch events"]);
-    exit;
+session_start();
+$client = new Google_Client();
+$client->setAuthConfig($apiKeyFile);
+$client->setScopes('https://www.googleapis.com/auth/calendar.readonly');
+$client->setApplicationName("Displayboard");
+$service = new Google_Service_Calendar($client);
+
+// https://developers.google.com/calendar/api/v3/reference/events/list
+$optParams = array(
+  'maxResults' => 40,
+  'orderBy' => 'startTime',
+  'singleEvents' => TRUE,
+  'timeMin' => date("Y-m-d"),
+);
+
+$results = $service->events->listEvents($calendarId, $optParams);
+
+$response = []; // Initialize an empty array
+foreach ($events->getItems() as $event) {
+  $start = $event->start->dateTime;
+  if (empty($start)) {
+    $start = $event->start->date;
+  }
+  $end = $event->start->dateTime;
+  if (empty($end)) {
+    $end = $event->start->date;
+  }
+  $response[] = [
+        "start" => $start,
+        "end" => $end,
+        "summary" => $event->getSummary()
+    ];
 }
+
+$response = json_encode($response, JSON_PRETTY_PRINT);
 
 // Save response to cache
 file_put_contents($cacheFile, $response);
@@ -110,7 +79,6 @@ file_put_contents($cacheFile, $response);
 // Output response
 header('Content-Type: application/json');
 echo $response;
- */
 
 ?>
 
