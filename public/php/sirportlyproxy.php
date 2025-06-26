28955f94d0ca7f5a49c80ae866ee0eb54617c83b
Dan Benton <dan@dogsbody.com>
2025-06-26 11:57:18 +0100

    Fix syntax error in sirportly proxy

--- public/php/sirportlyproxy.php
+++ public/php/sirportlyproxy.php
@@ -31,7 +31,7 @@ if (empty($apiHeaders)) {
     exit;
 }
 
-$SQL = 'SELECT%20COUNT%2Cusers.first_name%2Cusers.last_name%2Cstatus.name%2Cstatus.status_type%20FROM%20tickets%20WHERE%20statuses.status_type%20!%3D%201%20GROUP%20BY%20users.first_name%2Cusers.last_name%2Cstatus.name'
+$SQL = 'SELECT%20COUNT%2Cusers.first_name%2Cusers.last_name%2Cstatus.name%2Cstatus.status_type%20FROM%20tickets%20WHERE%20statuses.status_type%20!%3D%201%20GROUP%20BY%20users.first_name%2Cusers.last_name%2Cstatus.name';
 
 proxyRequest("$apiUrl/api/v2/tickets/spql?spql=$SQL", $apiHeaders, $cacheFile, $cacheDuration);
 
